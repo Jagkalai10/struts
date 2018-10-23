fa94c88293b8aafc882814a70c256a83c2ea4e44
jagkalai10 <jagkalai10>
2018-10-23 16:40:21 -0400

    Modernization from Struts to Spring MVC

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -1,8 +1,5 @@
 package org.superbiz.struts;
 
-import com.opensymphony.module.sitemesh.filter.PageFilter;
-import org.apache.struts2.dispatcher.ActionContextCleanUp;
-import org.apache.struts2.dispatcher.FilterDispatcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -10,37 +7,48 @@ import org.springframework.context.annotation.Bean;
 
 import javax.servlet.Filter;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static java.util.Collections.singletonList;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String... args) {
+
         SpringApplication.run(Application.class, args);
     }
 
-
-    @Bean
-    public FilterRegistrationBean filterDispatcher() {
-        return buildFilterRegistration(0, new FilterDispatcher());
-    }
-
-    @Bean
-    public FilterRegistrationBean actionContextCleanup() {
-        return buildFilterRegistration(1, new ActionContextCleanUp());
-    }
-
-    @Bean
-    public FilterRegistrationBean sitemeshPageFilter() {
-        return buildFilterRegistration(2, new PageFilter());
-    }
-
-
-    private FilterRegistrationBean buildFilterRegistration(int order, Filter filter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        registration.setUrlPatterns(singletonList("/*"));
-        registration.setOrder(order);
-        return registration;
-    }
+//
+//    @Bean
+//    public FilterRegistrationBean filterDispatcher() {
+//
+//        return buildFilterRegistration(0, new FilterDispatcher());
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean actionContextCleanup() {
+//        return buildFilterRegistration(1, new ActionContextCleanUp());
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean sitemeshPageFilter() {
+//
+//        return buildFilterRegistration(2, new PageFilter());
+//    }
+//
+//
+//    private FilterRegistrationBean buildFilterRegistration(int order, Filter filter) {
+//        FilterRegistrationBean registration = new FilterRegistrationBean();
+//
+//        registration.setFilter(filter);
+//        registration.setUrlPatterns(
+//                Arrays.asList(
+//                "/decorators/layout.jsp",
+//                "/displayUser.jsp",
+//                "/displayUsers.jsp"));
+//        registration.setOrder(order);
+//        return registration;
+//    }
 }
